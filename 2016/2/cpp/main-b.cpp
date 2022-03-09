fd531a916820a4c4f9c01b591247c37634aef4f7
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2022-03-09 10:10:08 +0100

    2016 - ex2

---
+++ 2016/2/cpp/main-b.cpp
@@ -0,0 +1,159 @@
+#define NOMINMAX
+#include <windows.h> // for nice formatted output
+// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences?redirectedfrom=MSDN
+
+#include <limits.h>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <string_view>
+#include <string>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+
+
+std::vector<std::string> split(const std::string& s, const std::string& delim = " ", const bool keepSplitting = true)
+{
+  std::vector<std::string> tokens;
+
+  size_t start = 0;
+  size_t end = 0;
+  while((end = s.find(delim, start)) != std::string::npos)
+  {
+    tokens.emplace_back(s.substr(start, end-start));
+    start = end + delim.size();
+    if (keepSplitting)
+    {
+      while (s.substr(start, delim.size()) == delim)
+      {
+        start += delim.size();
+      }
+    }
+  }
+  tokens.emplace_back(s.substr(start));
+
+  return tokens;
+}
+
+std::string trimmed(const std::string& s)
+{
+  auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
+  auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
+  return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
+}
+
+void solve(const std::vector<std::string>& rawInput)
+{
+  // std::cout << "Printing the input inside solution():" << std::endl;
+  // for (auto line : rawInput) {
+  //   std::cout << "Line: " << line << std::endl;
+  // }
+
+  std::string code;
+  uint64_t nr = 5;
+
+  for (const std::string& input : rawInput)
+  {
+    for (const char& c : input)
+    {
+      switch (c)
+      {
+        case 'U':
+          if (nr != 1 && nr != 2 && nr != 5 && nr != 4 && nr != 9)
+            if (nr == 3 || nr == 13) nr -= 2;
+            else nr -= 4;
+          break;
+        case 'D':
+          if (nr != 5 && nr != 10 && nr != 12 && nr != 13 && nr != 9)
+            if (nr == 11 || nr == 1) nr += 2;
+            else nr += 4;
+          break;
+        case 'L':
+          if (nr != 1 && nr != 2 && nr != 5 && nr != 10 && nr != 13) nr -= 1;
+          break;
+        case 'R':
+          if (nr != 1 && nr != 4 && nr != 9 && nr != 12 && nr != 13) nr += 1;
+          break;
+        default:
+          std::cout << "ERROR, should never reach this!" << std::endl;
+      }
+
+      // std::cout << "C=" << c << "  nr=" << nr << std::endl;
+    }
+
+    char cod = nr < 10 ? nr + 48 : nr + 55;
+    code = code + cod;
+  }
+
+  std::cout << "Code=" << code << std::endl;
+
+}
+
+void run(std::string_view filename)
+{
+  // Check if the file exists.
+  const fs::path filepath { filename };
+  if (!fs::exists(filepath))
+  {
+    std::cout << "File " << filepath.string() << " does not exist!" << std::endl;
+    return;
+  }
+
+  std::cout << "Running the program using the input file: " << filepath.string() << std::endl;
+
+  // Read the raw input and pass it in.
+  std::vector<std::string> rawInput;
+
+  std::ifstream file(filepath, std::ios::in);
+  for(std::string line; std::getline(file, line); )
+  {
+    // Simple line
+    // rawInput.emplace_back(line);
+
+    const std::string trimmedLine = trimmed(line);
+    if (!trimmedLine.empty() && trimmedLine != "" && trimmedLine != "\n")
+    {
+      rawInput.emplace_back(trimmedLine);
+    }
+  }
+
+  solve(rawInput);
+}
+
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+
+int main(int argc, char** argv)
+{
+  // Windows enable nice formatting output.
+  HANDLE handleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD consoleMode;
+  GetConsoleMode(handleOut, &consoleMode);
+  consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(handleOut, consoleMode);
+
+  // Parse arguments.
+  const char* INPUT_FILENAME = { "input.txt" };
+  std::string_view filename;
+  if (argc < 2)
+  {
+    // std::cout << "Missing filename argument!" << std::endl;
+    filename = INPUT_FILENAME;
+  }
+  else
+  {
+    filename = argv[1];
+  }
+
+  // Run the program.
+  run(filename);
+
+  return 0;
+}
