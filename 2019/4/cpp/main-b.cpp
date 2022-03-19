f8241fdaa74b7f63761bae1ab0f87f05c14369c9
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2022-03-19 15:12:47 +0100

    2019 - ex4

---
+++ 2019/4/cpp/main-b.cpp
@@ -0,0 +1,193 @@
+#define NOMINMAX
+#include <windows.h> // for nice formatted output
+// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences?redirectedfrom=MSDN
+
+#include <limits.h>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <string_view>
+#include <string>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+using Strings = std::vector<std::string>;
+using Strings2 = std::vector<Strings>;
+using Numbers = std::vector<uint64_t>;
+using Numbers2 = std::vector<Numbers>;
+using Pairs = std::vector<std::pair<int64_t,int64_t>>;
+using Pairs2 = std::vector<Pairs>;
+using Line = std::pair<std::pair<int64_t,int64_t>,std::pair<int64_t,int64_t>>;
+using Lines = std::vector<Line>;
+
+
+Strings split(const std::string& s, const std::string& delim = " ", const bool keepSplitting = true)
+{
+  Strings tokens;
+
+  size_t start = 0;
+  size_t end = 0;
+  while((end = s.find(delim, start)) != std::string::npos)
+  {
+    tokens.emplace_back(s.substr(start, end-start));
+    start = end + delim.size();
+    if (keepSplitting)
+    {
+      while (s.substr(start, delim.size()) == delim)
+      {
+        start += delim.size();
+      }
+    }
+  }
+  tokens.emplace_back(s.substr(start));
+
+  return tokens;
+}
+
+std::string trimmed(const std::string& s)
+{
+  auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
+  auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
+  return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
+}
+
+void solve(const Strings& rawInput)
+{
+  // std::cout << "Printing the input inside solution():" << std::endl;
+  // for (auto line : rawInput) {
+  //   std::cout << "Line: " << line << std::endl;
+  // }
+
+  if (rawInput.size() < 1)
+  {
+    std::cout << "RAW INPUT HAS NO ELEMENTS!" << std::endl;
+    return;
+  }
+
+  const Strings& input = split(rawInput[0], "-");
+  if (input.size() < 2)
+  {
+    std::cout << "INPUT HAS LESS THAN 2 ELEMENTS!" << std::endl;
+    return;
+  }
+  const std::string& lowerLimit = input[0];
+  const std::string& upperLimit = input[1];
+
+  uint64_t count = 0;
+  for (char i1 = '1'; i1 <= '9'; ++i1)
+  {
+    for (char i2 = i1; i2 <= '9'; ++i2)
+    {
+      for (char i3 = i2; i3 <= '9'; ++i3)
+      {
+        for (char i4 = i3; i4 <= '9'; ++i4)
+        {
+          for (char i5 = i4; i5 <= '9'; ++i5)
+          {
+            for (char i6 = i5; i6 <= '9'; ++i6)
+            {
+              const std::string& str {i1,i2,i3,i4,i5,i6};
+
+              // check if the string is within the range.
+              if (str < lowerLimit || str > upperLimit) continue;
+
+              // Check for doubles
+              bool hasOnlyDouble = false;
+              for (uint64_t k = 1; k < str.size()-2; ++k)
+              {
+                if (str[k] == str[k+1] &&
+                    str[k] != str[k-1] &&
+                    str[k+1] != str[k+2])
+                {
+                  hasOnlyDouble = true;
+                  break;
+                }
+              }
+              // Now check the last 2 chars
+              if (str[str.size()-1] == str[str.size()-2] &&
+                  str[str.size()-1] != str[str.size()-3] ||
+                  str[0] == str[1] && str[0] != str[2]) hasOnlyDouble = true;
+
+              if (hasOnlyDouble)
+              {
+                std::cout << str << std::endl;
+                count++;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  std::cout << "Count: " << count << std::endl;
+
+}
+
+void run(std::string_view filename)
+{
+  // Check if the file exists.
+  const fs::path filepath { filename };
+  if (!fs::exists(filepath))
+  {
+    std::cout << "File " << filepath.string() << " does not exist!" << std::endl;
+    return;
+  }
+
+  std::cout << "Running the program using the input file: " << filepath.string() << std::endl;
+
+  // Read the raw input and pass it in.
+  Strings rawInput;
+
+  std::ifstream file(filepath, std::ios::in);
+  for(std::string line; std::getline(file, line); )
+  {
+    // Simple line
+    // rawInput.emplace_back(line);
+
+    const std::string trimmedLine = trimmed(line);
+    if (!trimmedLine.empty() && trimmedLine != "" && trimmedLine != "\n")
+    {
+      rawInput.emplace_back(trimmedLine);
+    }
+  }
+
+  solve(rawInput);
+}
+
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+
+int main(int argc, char** argv)
+{
+  // Windows enable nice formatting output.
+  HANDLE handleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD consoleMode;
+  GetConsoleMode(handleOut, &consoleMode);
+  consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(handleOut, consoleMode);
+
+  // Parse arguments.
+  const char* INPUT_FILENAME = { "input.txt" };
+  std::string_view filename;
+  if (argc < 2)
+  {
+    // std::cout << "Missing filename argument!" << std::endl;
+    filename = INPUT_FILENAME;
+  }
+  else
+  {
+    filename = argv[1];
+  }
+
+  // Run the program.
+  run(filename);
+
+  return 0;
+}
