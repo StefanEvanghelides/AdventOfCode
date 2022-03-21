7201a0fdb91876043e41835db8a2b93c5ddd387b
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2022-03-22 00:19:48 +0100

    2016 - ex6

---
+++ 2016/6/cpp/main-b.cpp
@@ -0,0 +1,162 @@
+#define NOMINMAX
+#include <windows.h> // for nice formatted output
+// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences?redirectedfrom=MSDN
+
+#include <limits.h>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <string_view>
+#include <string>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+
+
+std::vector<std::string> split(const std::string& s, const std::string& delim = " ", const bool keepSplitting = true)
+{
+  std::vector<std::string> tokens;
+
+  size_t start = 0;
+  size_t end = 0;
+  while((end = s.find(delim, start)) != std::string::npos)
+  {
+    tokens.emplace_back(s.substr(start, end-start));
+    start = end + delim.size();
+    if (keepSplitting)
+    {
+      while (s.substr(start, delim.size()) == delim)
+      {
+        start += delim.size();
+      }
+    }
+  }
+  tokens.emplace_back(s.substr(start));
+
+  return tokens;
+}
+
+std::string trimmed(const std::string& s)
+{
+  auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
+  auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
+  return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
+}
+
+using Pair = std::pair<char, uint64_t>;
+
+void solve(const std::vector<std::string>& rawInput)
+{
+  // std::cout << "Printing the input inside solution():" << std::endl;
+  // for (auto line : rawInput) {
+  //   std::cout << "Line: " << line << std::endl;
+  // }
+
+  std::vector<std::map<char,uint64_t>> freqs;
+  const uint64_t size = rawInput[0].size();
+  // for (const std::string& line : rawInput)
+  // {
+  //   std::map<char,uint64_t> freq;
+  //   for (const char& c : line) freq[c]++;
+  //   freqs.push_back(freq);
+  // }
+
+  for (uint64_t i = 0; i < size; ++i)
+  {
+    freqs.emplace_back();
+    for (const std::string& line : rawInput)
+    {
+      freqs.back()[line[i]]++;
+    }
+  }
+
+  // processing time, I need to create arrays to sort them backwards.
+
+  using Pair = std::pair<char,uint64_t>;
+  for (const auto& freq : freqs)
+  {
+    std::vector<Pair> vec;
+    for (const auto& p : freq) vec.push_back(p);
+    if (vec.size() < 1)
+    {
+      std::cout << std::endl << "VECTOR HAS NO SIZE!" << std::endl;
+      continue;
+    }
+
+    // The only change necessary for part b) is to update the comparator.
+    sort(vec.begin(), vec.end(), [](const Pair&a, const Pair&b)
+    {
+      return a.second < b.second;
+    });
+    std::cout << vec[0].first;
+  }
+
+}
+
+void run(std::string_view filename)
+{
+  // Check if the file exists.
+  const fs::path filepath { filename };
+  if (!fs::exists(filepath))
+  {
+    std::cout << "File " << filepath.string() << " does not exist!" << std::endl;
+    return;
+  }
+
+  std::cout << "Running the program using the input file: " << filepath.string() << std::endl;
+
+  // Read the raw input and pass it in.
+  std::vector<std::string> rawInput;
+
+  std::ifstream file(filepath, std::ios::in);
+  for(std::string line; std::getline(file, line); )
+  {
+    // Simple line
+    // rawInput.emplace_back(line);
+
+    const std::string trimmedLine = trimmed(line);
+    if (!trimmedLine.empty() && trimmedLine != "" && trimmedLine != "\n")
+    {
+      rawInput.emplace_back(trimmedLine);
+    }
+  }
+
+  solve(rawInput);
+}
+
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+
+int main(int argc, char** argv)
+{
+  // Windows enable nice formatting output.
+  HANDLE handleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD consoleMode;
+  GetConsoleMode(handleOut, &consoleMode);
+  consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(handleOut, consoleMode);
+
+  // Parse arguments.
+  const char* INPUT_FILENAME = { "input.txt" };
+  std::string_view filename;
+  if (argc < 2)
+  {
+    // std::cout << "Missing filename argument!" << std::endl;
+    filename = INPUT_FILENAME;
+  }
+  else
+  {
+    filename = argv[1];
+  }
+
+  // Run the program.
+  run(filename);
+
+  return 0;
+}
