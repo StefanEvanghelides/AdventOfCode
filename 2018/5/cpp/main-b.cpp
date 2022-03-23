67a1da0a04a4e2e94c60e2ac0c78e3eef89b8ff8
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2022-03-23 16:22:24 +0100

    2018 - ex5

---
+++ 2018/5/cpp/main-b.cpp
@@ -0,0 +1,177 @@
+#define NOMINMAX
+#include <windows.h> // for nice formatted output
+// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences?redirectedfrom=MSDN
+
+#include <limits.h>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <algorithm>
+#include <string_view>
+#include <string>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+using Strings = std::vector<std::string>;
+using Strings2 = std::vector<Strings>;
+using Numbers = std::vector<uint64_t>;
+using Numbers2 = std::vector<Numbers>;
+
+
+Strings split(const std::string& s, const std::string& delim = " ", const bool keepSplitting = true)
+{
+  Strings tokens;
+
+  size_t start = 0;
+  size_t end = 0;
+  while((end = s.find(delim, start)) != std::string::npos)
+  {
+    tokens.emplace_back(s.substr(start, end-start));
+    start = end + delim.size();
+    if (keepSplitting)
+    {
+      while (s.substr(start, delim.size()) == delim)
+      {
+        start += delim.size();
+      }
+    }
+  }
+  tokens.emplace_back(s.substr(start));
+
+  return tokens;
+}
+
+std::string trimmed(const std::string& s)
+{
+  auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
+  auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
+  return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
+}
+
+
+void solve(const Strings& rawInput)
+{
+  // std::cout << "Printing the input inside solution():" << std::endl;
+  // for (auto line : rawInput) {
+  //   std::cout << "Line: " << line << std::endl;
+  // }
+
+  if (rawInput.size() < 1)
+  {
+    std::cout << "RAW INPUT HAS NO ELEMENTS!" << std::endl;
+    return;
+  }
+
+  uint64_t minSize = rawInput[0].size();
+  for (uint64_t p = 0; p <= 'z'-'a'; ++p)
+  {
+    std::string line = rawInput[0];
+
+    // remove all occurences of letter p+'a'
+    const char c1 = p + 'a';
+    line.erase(std::remove(line.begin(), line.end(), c1), line.end());
+    
+    // remove all occurences of letter p+'A'
+    const char c2 = p + 'A';
+    line.erase(std::remove(line.begin(), line.end(), c2), line.end());
+
+    uint64_t iter = 0;
+    bool hasChanged = false;
+    const uint64_t MAX_ITERATIONS = 100000;
+    do
+    {
+      hasChanged = false;
+
+      // Update the line
+      for (uint64_t i = 0; i < line.size()-1; ++i)
+      {
+        if (abs(line[i] - line[i+1]) == 32)
+        {
+          // delete the 2 chars
+          line.erase(i,1);
+          line.erase(i,1);
+          hasChanged = true;
+          break;
+        }
+      }
+
+      iter++;
+
+    } while(hasChanged && iter < MAX_ITERATIONS);
+
+    // std::cout << "Final string size: " << line.size() << std::endl;
+
+    if (line.size() < minSize) minSize = line.size();
+
+  }
+
+  std::cout << "Min size: " << minSize << std::endl;
+
+}
+
+void run(std::string_view filename)
+{
+  // Check if the file exists.
+  const fs::path filepath { filename };
+  if (!fs::exists(filepath))
+  {
+    std::cout << "File " << filepath.string() << " does not exist!" << std::endl;
+    return;
+  }
+
+  std::cout << "Running the program using the input file: " << filepath.string() << std::endl;
+
+  // Read the raw input and pass it in.
+  Strings rawInput;
+
+  std::ifstream file(filepath, std::ios::in);
+  for(std::string line; std::getline(file, line); )
+  {
+    // Simple line
+    // rawInput.emplace_back(line);
+
+    const std::string trimmedLine = trimmed(line);
+    if (!trimmedLine.empty() && trimmedLine != "" && trimmedLine != "\n")
+    {
+      rawInput.emplace_back(trimmedLine);
+    }
+  }
+
+  solve(rawInput);
+}
+
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+
+int main(int argc, char** argv)
+{
+  // Windows enable nice formatting output.
+  HANDLE handleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD consoleMode;
+  GetConsoleMode(handleOut, &consoleMode);
+  consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(handleOut, consoleMode);
+
+  // Parse arguments.
+  const char* INPUT_FILENAME = { "input.txt" };
+  std::string_view filename;
+  if (argc < 2)
+  {
+    // std::cout << "Missing filename argument!" << std::endl;
+    filename = INPUT_FILENAME;
+  }
+  else
+  {
+    filename = argv[1];
+  }
+
+  // Run the program.
+  run(filename);
+
+  return 0;
+}
