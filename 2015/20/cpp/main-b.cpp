f3e48cb6df21fb97a1106441c9977deea257d731
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2022-04-09 21:50:21 +0200

    2015 - ex20

---
+++ 2015/20/cpp/main-b.cpp
@@ -0,0 +1,176 @@
+#define NOMINMAX
+#include <windows.h> // for nice formatted output
+// https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences?redirectedfrom=MSDN
+
+#include <limits.h>
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+#include <string_view>
+#include <string>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+using Strings = std::vector<std::string>;
+using Strings2 = std::vector<Strings>;
+using Numbers = std::vector<uint64_t>;
+using Numbers2 = std::vector<Numbers>;
+using Bools = std::vector<bool>;
+using Bools2 = std::vector<Bools>;
+using Pairs = std::vector<std::pair<int64_t,int64_t>>;
+using Pairs2 = std::vector<Pairs>;
+using Line = std::pair<std::pair<int64_t,int64_t>,std::pair<int64_t,int64_t>>;
+using Lines = std::vector<Line>;
+
+
+Strings split(const std::string& s, const std::string& delim = " ", const bool keepSplitting = true)
+{
+  Strings tokens;
+
+  size_t start = 0;
+  size_t end = 0;
+  while((end = s.find(delim, start)) != std::string::npos)
+  {
+    tokens.emplace_back(s.substr(start, end-start));
+    start = end + delim.size();
+    if (keepSplitting)
+    {
+      while (s.substr(start, delim.size()) == delim)
+      {
+        start += delim.size();
+      }
+    }
+  }
+  tokens.emplace_back(s.substr(start));
+
+  return tokens;
+}
+
+std::string trimmed(const std::string& s)
+{
+  auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
+  auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
+  return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
+}
+
+
+constexpr uint64_t MULTIPLIER = 11;
+constexpr uint64_t MAX_ITERATIONS = 1000000;
+constexpr uint64_t MAX_HOUSES = 50;
+
+
+uint64_t sumFactors(const uint64_t& n, const bool& innerOnly = false, const uint64_t& maxHouses = MAX_HOUSES)
+{
+  if (n == 1) return n;
+
+  // Always include 1 and the number itself if innerOnly is false.
+  uint64_t result = (innerOnly ? 0 : n + (n <= maxHouses));
+
+  for (uint64_t i = 2; i <= sqrt(n); ++i)
+  {
+    // Check if n divides by i. If so, then add it + the remaining number.
+    // Add the remaiing only if it is different from i.
+    if (n % i == 0)
+    {
+      if (n/i <= maxHouses) result += i;
+      if (i != n/i && i <= maxHouses) result += n/i;
+    }
+  }
+
+  return result;
+}
+
+void solve(const Strings& rawInput)
+{
+  // std::cout << "Printing the input inside solution():" << std::endl;
+  // for (auto line : rawInput) {
+  //   std::cout << "Line: " << line << std::endl;
+  // }
+
+  // Multiple numbers because of multiple test scenarios
+  for (const std::string& input : rawInput)
+  {
+    // Fetch the number and divide by the multiplier.
+    // This is because we don't need the multiplied numbers here.
+    const uint64_t number = (uint64_t)std::ceil((double)std::stoull(input) / MULTIPLIER - 0.1);
+    std::cout << "Number: " << number << std::endl;
+
+    // Now this is a simple problem to compute the sum of factors.
+    // Check which is the first one greater than our number.
+    uint64_t i = 1;
+    while (i < MAX_ITERATIONS && number > sumFactors(i))
+    {
+      i++;
+    }
+    std::cout << "First house: " << i << std::endl;
+  }
+}
+
+void run(std::string_view filename)
+{
+  // Check if the file exists.
+  const fs::path filepath { filename };
+  if (!fs::exists(filepath))
+  {
+    std::cout << "File " << filepath.string() << " does not exist!" << std::endl;
+    return;
+  }
+
+  std::cout << "Running the program using the input file: " << filepath.string() << std::endl;
+
+  // Read the raw input and pass it in.
+  Strings rawInput;
+
+  std::ifstream file(filepath, std::ios::in);
+  for(std::string line; std::getline(file, line); )
+  {
+    // Simple line
+    // rawInput.emplace_back(line);
+
+    const std::string trimmedLine = trimmed(line);
+    if (!trimmedLine.empty() && trimmedLine != "" && trimmedLine != "\n")
+    {
+      rawInput.emplace_back(trimmedLine);
+    }
+  }
+
+  solve(rawInput);
+}
+
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+
+int main(int argc, char** argv)
+{
+  // Windows enable nice formatting output.
+  HANDLE handleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD consoleMode;
+  GetConsoleMode(handleOut, &consoleMode);
+  consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(handleOut, consoleMode);
+
+  // Parse arguments.
+  const char* INPUT_FILENAME = { "input.txt" };
+  std::string_view filename;
+  if (argc < 2)
+  {
+    // std::cout << "Missing filename argument!" << std::endl;
+    filename = INPUT_FILENAME;
+  }
+  else
+  {
+    filename = argv[1];
+  }
+
+  // Run the program.
+  run(filename);
+
+  return 0;
+}
