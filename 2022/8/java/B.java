3caa1ad029f96b591b65c7bd785cf1283fa68537
Stefan Evanghelides <stefan.evanghelides@fivetran.com>
2022-12-08 11:46:31 +0100

    2022 - 8 java

---
+++ 2022/8/java/B.java
@@ -0,0 +1,227 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class B {
+
+    // Simple generic tree class, implemented as a struct.
+    private static class Board {
+        private int n;
+        private int m;
+        private int[][] b;
+        private boolean[][] vis;
+
+        Board(int n, int m) {
+            this.n = n;
+            this.m = m;
+            b = new int[n][m];
+            vis = new boolean[n][m];
+
+            // Mark the outskirts visible. Rest not visible for now.
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < m; ++j)
+                    vis[i][j] = (i == 0 || j == 0 || i == n-1 || j == n-1);
+        }
+
+        public void setRow(int idx, String row) {
+            for (int i = 0; i < row.length(); ++i) {
+                int val = row.charAt(i) - '0';
+                b[idx][i] = val;
+            }
+        }
+
+        public void print() {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    System.out.print(b[i][j]);
+                }
+                System.out.println();
+            }
+        }
+
+        public void printVisible() {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    System.out.print(vis[i][j] ? 1 : 0);
+                }
+                System.out.println();
+            }
+        }
+
+        public int countVisible() {
+            markVisible();
+
+            int total = 0;
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < m; ++j)
+                    if (vis[i][j])
+                        total++;
+
+            return total;
+        }
+
+        private void markVisible() {
+            // We only need to check the insides.
+
+            // Left to right
+            for (int i = 1; i < n-1; ++i) {
+                int curMax = b[i][0];
+                for (int j = 1; j < m-1; ++j) {
+                    if (b[i][j] > curMax) {
+                        curMax = b[i][j];
+                        vis[i][j] |= true;
+                    }
+                }
+            }
+            // Right to left
+            for (int i = 1; i < n-1; ++i) {
+                int curMax = b[i][m-1];
+                for (int j = m-2; j > 0; --j) {
+                    if (b[i][j] > curMax) {
+                        curMax = b[i][j];
+                        vis[i][j] |= true;
+                    }
+                }
+            }
+            // Top to bottom
+            for (int j = 1; j < m-1; ++j) {
+                int curMax = b[0][j];
+                for (int i = 1; i < n-1; ++i) {
+                    if (b[i][j] > curMax) {
+                        curMax = b[i][j];
+                        vis[i][j] |= true;
+                    }
+                }
+            }
+            // Bottom up
+            for (int j = 1; j < m-1; ++j) {
+                int curMax = b[n-1][j];
+                for (int i = n-2; i > 0; --i) {
+                    if (b[i][j] > curMax) {
+                        curMax = b[i][j];
+                        vis[i][j] |= true;
+                    }
+                }
+            }
+        }
+
+        public int highestScenicScore() {
+            int highestScore = 0;
+            for (int i = 1; i < n-1; ++i) {
+                for (int j = 1; j < m-1; ++j) {
+                    // Now search in all 4 directions
+                    int scenicScore = countFromPointBottom(i, j, b[i][j])
+                                    * countFromPointTop(i, j, b[i][j])
+                                    * countFromPointLeft(i, j, b[i][j])
+                                    * countFromPointRight(i, j, b[i][j]);
+
+                    // Check the computed score
+                    if (scenicScore > highestScore)
+                        highestScore = scenicScore;
+                }
+            }
+            return highestScore;
+        }
+
+        private int countFromPointRight(int i, int j, int val) {
+            int count = 0;
+            for (int k = j+1; k < m; ++k) {
+                count++;
+                if (b[i][k] >= val)
+                    break;
+            }
+            return count;
+        }
+
+        private int countFromPointLeft(int i, int j, int val) {
+            int count = 0;
+            for (int k = j-1; k >= 0; --k) {
+                count++;
+                if (b[i][k] >= val)
+                    break;
+            }
+            return count;
+        }
+
+        private int countFromPointBottom(int i, int j, int val) {
+            int count = 0;
+            for (int k = i+1; k < n; ++k) {
+                count++;
+                if (b[k][j] >= val)
+                    break;
+            }
+            return count;
+        }
+
+        private int countFromPointTop(int i, int j, int val) {
+            int count = 0;
+            for (int k = i-1; k >= 0; --k) {
+                count++;
+                if (b[k][j] >= val)
+                    break;
+            }
+            return count;
+        }
+    
+    }
+
+    // O(N^3) time and O(N^2) space. For each element, about 4N extra passes.
+    private static void solve(String[] rawInput) {
+        // Guard
+        if (rawInput == null || rawInput.length < 1) {
+            throw new Error("Raw input is NULL!");
+        }
+
+        // System.out.println("testing input...");
+        // for (String inp : rawInput) {
+        //     System.out.println("Line: " + inp);
+        // }
+
+        int n = rawInput.length;
+        int m = rawInput[0].length();
+        Board b = new Board(n, m);
+        for (int i = 0; i < n; ++i) {
+            String inp = rawInput[i];
+            b.setRow(i, inp);
+        }
+
+        // b.print();
+
+        System.out.println("Count visible: " + b.countVisible());
+
+        // b.printVisible();
+
+        System.out.println("Highest scenic score: " + b.highestScenicScore());
+    }
+
+    private static void run(String filename) {
+        // Guard
+        if (filename == null || filename.isEmpty()) {
+            throw new Error("Filename is NULL!");
+        }
+
+        // Open file and read it.
+        String content = null;
+        try {
+            content = Files.readString(Paths.get(filename));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // Now split this in multiple rows, if exist.
+        String[] rawInput = content.split(System.lineSeparator());
+
+        solve(rawInput);
+    }
+
+    private static final String DEFAULT_INPUT_FILENAME = "input";
+
+    public static void main(String[] args) {
+        // Parse argument.
+        String filename = null;
+        filename = args != null && args.length > 0 ? args[0] : DEFAULT_INPUT_FILENAME;
+
+        // Run the code.
+        run(filename);
+    }
+}
